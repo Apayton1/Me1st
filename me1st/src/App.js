476f3931f4133a1af87906091885c01f99282357
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-10-05 15:49:19 -0400

    initial commit and eslint set-up

---
+++ me1st/src/App.js
@@ -0,0 +1,17 @@
+// import React from "react";
+import {Route} from "react-router-dom";
+import {HomePage} from "./Components/pages/HomePage";
+import {LoginPage} from "./Components/pages/LoginPage";
+
+const App = () => <div>
+
+  <Route path = "/" exact component = {HomePage}/>
+  <Route path = "/login" exact component = {LoginPage}/>
+  
+</div>
+
+
+
+
+
+export default App;
