cb67c75f3d089a695e6497e7920e2ced7f404214
Aquila Payton <44341215+Apayton1@users.noreply.github.com>
2019-10-14 14:20:56 -0400

    rebuild of app/new base components for shop added/ routes for components set up

--- me1st/src/App.js
+++ me1st/src/App.js
@@ -1,19 +1,40 @@
-import React from "react";
-import {Route} from "react-router-dom";
-import HomePage from "./Components/pages/HomePage";
-import LoginPage from "./Components/pages/LoginPage";
+import React, { Component } from 'react';
+import {Switch, Route} from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+import Home from "./components/Home";
 
-const App = () => 
+export default class App extends Component {
+  render() {
+    return (
 
-<div className = "ui container">
+      <React.Fragment>
+        <h3> Hello From App </h3>
+        <Navbar/>
+        <Switch>
+        <Route exact path = "/" component = {Home}>
+          </Route>
 
-  <Route path = "/" exact component = {HomePage}/>
-  <Route path = "/login" exact component = {LoginPage}/>
-  
-</div>
+          <Route path = "/shop" component = {ProductList}>
+          </Route>
 
+          <Route path = "/details" component = {Details}>
+          </Route>
 
+          <Route path = "/cart" component = {Cart}>
+          </Route>
 
+          <Route component = {Default}>
+          </Route>
 
+        </Switch>
+        
+      </React.Fragment>
 
-export default App;
+    )
+  }
+}
